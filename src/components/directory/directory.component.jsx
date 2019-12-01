64a8ce5225feed7eca06792a8f9c8f18fb3a2aba
Shazi Rahim <shazirahim@shazis-mbp.lan>
2019-11-30 21:59:48 -0500

    updated menu-items to navigate with router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
-import MenuItem from '../menu-item/menu-item.component';
+import MenuItem from "../menu-item/menu-item.component";
 
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 export default class Directory extends Component {
   constructor() {
@@ -47,13 +47,11 @@ export default class Directory extends Component {
   }
   render() {
     return (
-        <div className="directory-menu">
-            {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-                ))
-            }
-        </div>
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
     );
   }
 }
