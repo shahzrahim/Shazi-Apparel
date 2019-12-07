c3c5df6130af94029955b34f1d9ae0648f0f3f19
Shazi Rahim <shazirahim@shazis-mbp.lan>
2019-12-07 15:38:42 -0500

    fixed but updated

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,46 +1,32 @@
-import React, { Component } from "react";
-import "./sign-in.styles.scss";
-import { FormInput } from "../form-input/form-input.component";
-import { CustomButton } from "../custom-button/custom-button.component";
-import { auth, signInWithGoogle, createUserProfileDocument } from "../../firebase/firebase.utils";
+import React from 'react';
 
-export default class SignIn extends Component {
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+      password: ''
     };
   }
 
   handleSubmit = async event => {
     event.preventDefault();
 
-    const { displayName, email, password, confirmPassword } = this.state;
-
-    if (password !== confirmPassword) {
-      alert("passwords don't match");
-      return;
-    }
+    const { email, password } = this.state;
 
     try {
-      
-      const { user } = await auth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-
-      await createUserProfileDocument(user, { displayName });
-
-      this.setState({
-        displayName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      });
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
@@ -49,39 +35,34 @@ export default class SignIn extends Component {
 
     this.setState({ [name]: value });
   };
+
   render() {
     return (
-      <div className="sign-in">
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
+            name='email'
+            type='email'
             handleChange={this.handleChange}
-            label="email"
+            value={this.state.email}
+            label='email'
             required
           />
           <FormInput
-            type="password"
-            name="password"
+            name='password'
+            type='password'
             value={this.state.password}
-            onChange={this.handleChange}
             handleChange={this.handleChange}
-            label="password"
+            label='password'
             required
           />
-          <div className="buttons">
-            <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton
-              type="submit"
-              onClick={signInWithGoogle}
-              isGoogleSignIn
-            >
-              Sign In with Google
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
@@ -89,3 +70,5 @@ export default class SignIn extends Component {
     );
   }
 }
+
+export default SignIn;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from 'react';
 
-import {SHOP_DATA} from './shop.data'
+import SHOP_DATA from './shop.data.js';
 
-import {CollectionPreview} from '../../components/collection-preview/collection-preview.component'
+import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-export default class ShopPage extends Component {
+class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
@@ -12,14 +12,17 @@ export default class ShopPage extends Component {
       collections: SHOP_DATA
     };
   }
+
   render() {
-    const {collections} = this.state;
-    return (<div className='shop-page'>
-        {
-            collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))            
-        }
-    </div>);
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
+
+export default ShopPage;
