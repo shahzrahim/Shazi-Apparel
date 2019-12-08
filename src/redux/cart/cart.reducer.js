f9d3b14c683df7fb7b846eddeb68938811745fef
Shazi Rahim <shazirahim@shazis-mbp.lan>
2019-12-08 00:35:24 -0500

    updated cart item clear functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
 import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
+import { clearItemFromCart } from './cart.actions';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,6 +19,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+          )
+      }
     default:
       return state;
   }
