75016b01bb9ed6bde56a780d52e7566969b95659
Shazi Rahim <shazirahim@shazis-mbp.lan>
2019-12-08 21:34:24 -0500

    implemented redux for directory state, created directory selector for sections, configured directory component with connect and createStructuredSelector

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import asyncSessionStorage from 'redux-persist/lib/storage/session';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -13,7 +14,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
+
 });
 
 export default persistReducer(persistConfig, rootReducer);
