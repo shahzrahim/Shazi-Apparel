d2bb88a1800b8fc230c2d7b9e5a7ae87784037f9
Shazi Rahim <shazirahim@shazis-mbp.lan>
2019-12-08 21:06:35 -0500

    added redux-persist, configured root-reducer and store with persistStore, and configured index.js with persistGate for session storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import asyncSessionStorage from 'redux-persist/lib/storage/session';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage: asyncSessionStorage, 
+  whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
